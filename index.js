a3a5bf90e4c86310b6b47ab5dc6a9943691cf818
Paul Wong <paulwcy@gmail.com>
2016-10-14 08:26:17 +1100

    promise and callback compatible

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 'use strict';
 
 import { NativeModules } from 'react-native';
+var Q = require('q');
 
 var RNPinch = {
-    fetch: function (url, obj, callback){
+    fetch: function (url, obj, callback) {
+        var deferred = Q.defer();
         NativeModules.RNPinch.fetch(url, obj, (err, res) => {
-            callback(err, res);
+
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(res);
+            }
+
+            deferred.promise.nodeify(callback);
         });
+        return deferred.promise;
     }
 };
 
